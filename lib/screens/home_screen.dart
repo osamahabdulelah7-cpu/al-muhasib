a46252c1533b2a7402f046636fe33344e28f546c
osamahabdulelah7-cpu <osamahabdulelah7@gmail.com>
2026-09-15 01:18:10 +0300

    Build Al-Mohaseb APK

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'providers/account_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AccountProvider()..loadInitialData(),
+      child: const AlMuhasibApp(),
+    ),
+  );
+}
+
+class AlMuhasibApp extends StatelessWidget {
+  const AlMuhasibApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'تطبيق المحاسب',
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ar', ''),
+      supportedLocales: const [Locale('ar', '')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        useMaterial3: false,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
